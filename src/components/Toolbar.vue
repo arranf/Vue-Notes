0af5e797141c34c1f8d0cbae65d3cd7d95ef0934
Arran France <arranf@gmail.com>
2017-01-29 22:24:08 +0000

    Add signout to toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -7,16 +7,31 @@
     <i @click="deleteNote"
     class="glyphicon glyphicon-remove"
     :class="[{remove: isCurrentActiveAccessible}, {disabled: !isCurrentActiveAccessible}]"></i>
+    <i @click="signOut" class="glyphicon glyphicon-off signOut"></i>
   </div>
 </template>
 
 <script>
 import {mapActions}  from 'vuex'
+import firebase from 'firebase'
+import router from '../config/router'
 
 export default {
-    methods: mapActions([
-        'addNote', 'deleteNote', 'toggleFavourite', 'deleteNote'
-    ]),
+    methods: {
+      ...mapActions([
+        'addNote', 'deleteNote', 'toggleFavourite', 'deleteNote, signOut'
+      ]),
+      signOut() {
+        firebase.auth().signOut().then(this.onSignOut, this.onError);
+      },
+      onSignOut() {
+        console.log('onSignOut');
+        router.push({path: '/', query: {ref: 'signout'}})
+      },
+      onError() {
+        console.error('onError');
+      }
+    },
     computed: {
         isFavourite() {
             return (this.$store.state.activeNote == undefined) ? false :this.$store.state.activeNote.favourite
@@ -28,7 +43,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .plus:hover {
     color: #37AB76;
 }
@@ -53,5 +68,9 @@ export default {
 .disabled {
     opacity: 0.5 !important;
 }
+
+  .signOut:hover {
+    color: #FBE7E7;
+  }
 
 </style>
