5e3e29833ca7f6af6d56fdb77da343b808d7708a
Arran France <arranf@gmail.com>
2017-02-01 12:19:02 +0000

    Add Github auth

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -19,7 +19,7 @@
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 //      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
 //      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-//      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     tosUrl: '/tos',
